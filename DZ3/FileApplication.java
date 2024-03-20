6ebafcccabb4307b32e584b8b45521f6c8c3a51a
Фархад <58259681+toporicom@users.noreply.github.com>
2024-03-20 15:01:14 +0300

    Update FileApplication.java

--- DZ3/FileApplication.java
+++ DZ3/FileApplication.java
@@ -1,6 +1,7 @@
 package DZ3;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Files;
@@ -11,26 +12,26 @@ public class FileApplication {
         System.out.println("Сколько файлов вы хотите склеить?");
         int num = Integer.parseInt(scanner.nextLine());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        File file = new File("C:\\Users\\topor\\IdeaProjects\\untitled\\src\\DZ3\\final.txt");
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-
+        System.out.println("Введите путь к файлу");
+        String file1 = reader.readLine();
+        File file = new File(Paths.get(file1).getParent() + "\\answer.txt");
         FileOutputStream mainFile = new FileOutputStream(file);
         for (int i = 0; i < num; i++) {
-            System.out.println("Введите путь к файлу");
-            String file1 = reader.readLine();
+            if (i != 0){
+                System.out.println("Введите путь к файлу");
+                file1 = reader.readLine();
+            }
             FileInputStream fileInputStream = new FileInputStream(file1);
-            byte[] buffer = new byte[fileInputStream.available()]; // буфер размером с файл
+            byte[] buffer = new byte[fileInputStream.available()];
             while (fileInputStream.available() > 0) {
-                fileInputStream.read(buffer); // считываешь буфер и заносишь туда байты из файла
+                fileInputStream.read(buffer);
             }
 
             mainFile.write(buffer);
             fileInputStream.close();
         }
 
-        System.out.println("Written content in file:\n"+ new String(Files.readAllBytes(Path.of("C:\\Users\\topor\\IdeaProjects\\untitled\\src\\DZ3\\final.txt"))));
+        System.out.println(new String(Files.readAllBytes(Path.of(Paths.get(file1).getParent() + "\\answer.txt"))));
         mainFile.close();
     }
 }
